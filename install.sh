40065efc4c779540e8b9b8eaafe50b4ab01a9669
Kristofer Racsa <50684527+nisheri-ascar@users.noreply.github.com>
2023-01-01 14:19:17 +0800

    update

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
-# download
-curl https://github.com/ZhymabekRoman/proot-static/archive/refs/tags/1.0.tar.gz > $TMPDIR/file.tar.gz
+#!/system/bin/sh
+mkdir folder
+# Download proot-static
+curl https://github.com/ZhymabekRoman/proot-static/archive/refs/tags/1.0.tar.gz > ./folder/file.tar.gz
+cd folder
+# Untar
+tar -xvf file.tar.gz
+
