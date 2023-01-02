e9520cd08796afd59963a3cc782d1f5649cb5adf
nisheri-ascar <kertkert704@gmail.com>
2023-01-02 08:44:57 +0800

    Add bare minimum working installation

--- install.sh
+++ install.sh
@@ -1,8 +1,22 @@
 #!/system/bin/sh
-mkdir folder
-# Download proot-static
-curl https://github.com/ZhymabekRoman/proot-static/archive/refs/tags/1.0.tar.gz > ./folder/file.tar.gz
-cd folder
+mkdir rootfs
+# Download proot-static and debian rootfs.
+#curl -L https://github.com/ZhymabekRoman/proot-static/archive/refs/tags/1.0.tar.gz > $TMPDIR/proot.tar.gz
+#curl -L https://github.com/termux/proot-distro/releases/download/v3.3.0/debian-arm-pd-v3.3.0.tar.xz > $TMPDIR/rootfs.tar.xz
 # Untar
-tar -xvf file.tar.gz
+tar -xvf $TMPDIR/proot.tar.gz 
+tar -xvf $TMPDIR/rootfs.tar.xz -C rootfs/
+# Nameserver fix
+echo "nameserver 8.8.8.8" > rootfs/etc/resolv.conf
+# Write in ~/.bashrc that installs packages update stuff, in future might just add cronjob where it automatically does it every week.
+cat > rootfs/root/.bashrc <<- EOM
+#!/bin/sh
+apt update
+apt upgrade -y
+apt install gnupg gnupg2 -y
+wget https://itai-nelken.github.io/weekly-box86-debs/debian/box86.list -O /etc/apt/sources.list.d/box86.list
+wget -qO- https://itai-nelken.github.io/weekly-box86-debs/debian/KEY.gpg | apt-key add -
+apt update && apt install box86 tigervnc-standalone-server -y
 
+EOM
+bash run.sh
