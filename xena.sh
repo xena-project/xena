2f97a39ca3fe9442fab7679675785028b850d342
nisheri-ascar <kertkert704@gmail.com>
2023-01-03 18:10:58 +0800

    Initial variable stuff and few modifications

--- xena.sh
+++ xena.sh
@@ -1,8 +1,18 @@
 #!/system/bin/sh
 
-if [ ! -f rootfs/.installed ]; #Im sure this is a bad way of doing this, there must be a better way implementing this.
+#set -e -u
+
+#########
+# Global environments
+WINE_VERSION="7.1"
+ROOTFS_PATH="$HOME/.xena"
+#PROOT_BIN=""
+#########
+
+if [ ! -f $ROOTFS_PATH/.installed ]; #Im sure this is a bad way of doing this, there must be a better way implementing this.
 then
-mkdir rootfs
+mkdir $ROOTFS_PATH
+
 
 # Download proot-static and debian rootfs.                                  # TODO: Add --clean switch incase user encounters corrupted download.
 if [ ! -f $TMPDIR/proot.tar.gz ];
@@ -16,19 +26,20 @@ then
 fi
 
 # Download static busybox
-curl https://busybox.net/downloads/binaries/1.21.1/busybox-armv7l > rootfs/busybox
-chmod 777 rootfs/busybox
+mkdir $ROOTFS_PATH/bin
+curl https://busybox.net/downloads/binaries/1.21.1/busybox-armv7l > $ROOTFS_PATH/bin/busybox
+chmod 777 $ROOTFS_PATH/bin/busybox
 
 # Untar
 tar -xf $TMPDIR/proot.tar.gz 
 # Using this method to avoid symlink errors.
-proot-static-1.0/proot_static -0 -l -w / -r rootfs -b $TMPDIR/:/tmp /busybox tar -xf /tmp/rootfs.tar.xz
+proot-static-1.0/proot_static -0 -l -w / -r $ROOTFS_PATH -b $TMPDIR/:/tmp /bin/busybox tar -xf /tmp/rootfs.tar.xz
 
 # DNS fix
-echo "nameserver 8.8.8.8" > rootfs/etc/resolv.conf
+echo "nameserver 8.8.8.8" > $ROOTFS_PATH/etc/resolv.conf
 
-#
-echo "Message here" > rootfs/etc/motd
+# TODO: Add MOTD, i have no idea what to write something friendly
+echo "A" > $ROOTFS_PATH/etc/motd
 
 # Create VNC server start script
 # TODO: Make connection only accesable to localhost
@@ -36,10 +47,10 @@ echo "Message here" > rootfs/etc/motd
 cat > rootfs/bin/startvnc <<- EOM
 Xvnc -SecurityTypes none -listen tcp -ZlibLevel=0 -ImprovedHextile=0 -CompareFB=0 -geometry 1024x600 :1 > /dev/null & 
 EOM
-chmod 777 rootfs/bin/startvnc
+chmod 777 $ROOTFS_PATH/bin/startvnc
 
 # Write in ~/.bashrc that installs packages update stuff, in future might just add cronjob where it automatically does it every week.
-cat > rootfs/root/.bashrc <<- EOM
+cat > $ROOTFS_PATH/root/.bashrc <<- EOM
 #!/bin/sh
 if [ ! -f /root/.FirstRunDone ];
 then
@@ -52,33 +63,35 @@ then
 	touch /root/.FirstRunDone
 fi
 
-cat /etx/motd
+cat /etc/motd
 startvnc
 EOM
 
 # Self Explanatory
-printf "#!/bin/sh\nbox86 /opt/wine-devel/bin/wine $@" > /bin/wine
-printf "#!/bin/sh\nbox86 /opt/wine-devel/bin/wineserver $@" > /bin/wineserver
+printf "#!/bin/sh\nbox86 /opt/wine-devel/bin/wine $@" > $ROOTFS_PATH/bin/wine
+printf "#!/bin/sh\nbox86 /opt/wine-devel/bin/wineserver $@" > $ROOTFS_PATH/bin/wineserver
 
-touch rootfs/.installed # Wow best way to check if its installed
+touch $ROOTFS_PATH/.installed # Wow best way to check if its installed
 
 
 fi
 
+# Its prooting time
+# TODO: Optional `--proot-command` flag or similar.
 command="./proot-static-1.0/proot_static"
 command+=" --kill-on-exit"
 command+=" --link2symlink"
 command+=" -0"
-command+=" -r rootfs/"
+command+=" -r $ROOTFS_PATH"
 command+=" -b /dev"
 command+=" -b /proc"
 command+=" -b /sys"
 command+=" -b /data/data/com.termux"
-command+=" -b /:/host-rootfs"
 command+=" -b /sdcard"
 command+=" -b /storage"
 command+=" -b /mnt"
 command+=" -b $TMPDIR/:/tmp"
+#TODO: Automatically find XXXX-XXXX in /storage and mount it in /external for ease
 command+=" -b /storage/9A03-171A/Android/data/com.termux/files/:/external"
 command+=" -w /root"
 command+=" /usr/bin/env -i"
